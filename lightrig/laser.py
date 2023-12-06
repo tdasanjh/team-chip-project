e48ed1db89c3f87717a02b07476312e50e635bce
Tamanna Dasanjh <bp22325@bristol.ac.uk>
2023-12-06 16:56:06 +0000

    change from query to write laser commands

--- lightrig/laser.py
+++ lightrig/laser.py
@@ -15,20 +15,22 @@ class Laser:
 		return
 
 	def laser_enable(self):
-		response = self.laser.query(f"CH{self.channel}:ENABLE").strip()
-		if not response == f"CH{self.channel}:OK":
-			raise Exception(f"Error: {response}")
-		return
+		# response = self.laser.query(f"CH{self.channel}:ENABLE").strip()
+		# if not response == f"CH{self.channel}:OK":
+		# 	raise Exception(f"Error: {response}")
+		# return
+	    self.switch_on()
 
 	def switch_on(self):
-		response = self.laser.query(f"CH{self.channel}:ENABLE").strip()
-		if not response == "CH{:}:OK".format(self.channel):
-			raise Exception("Error: {}".format(response))
-		return
+		self.laser.write(f"CH{self.channel}:ENABLE")  # .strip()
+		# if not response == "CH{:}:OK".format(self.channel):
+		# 	raise Exception("Error: {}".format(response))
+		# return
 
 	def set_wavelength(self, w):
-		self.laser_enable()
-		response = self.laser.query(f"CH{self.channel}:L={w}").strip()
-		if not response == "CH{:}:OK".format(self.channel):
-			raise Exception("Error: {}".format(response))
-		return
+		self.switch_on()
+		self.laser.write(f"CH{self.channel}:L={w}")
+		# response = self.laser.query(f"CH{self.channel}:L={w}").strip()
+		# if not response == "CH{:}:OK".format(self.channel):
+		#	raise Exception("Error: {}".format(response))
+		# return
