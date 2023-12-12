c19a69c1a89f20f3e526745aef2012a89cc2df8e
Tamanna Dasanjh <bp22325@bristol.ac.uk>
2023-12-12 15:27:00 +0000

    committing changes made yesterday

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-file_path = "C:/Users/IT086179/Desktop/Team_Chip_1_2023/ltphotonics_Freddie_6Dec/team-chip-project/wavelength_sweeps"
+file_path = "C:/Users/IT086179/Desktop/Team_Chip_1_2023/ltphotonics_Freddie_6Dec/team-chip-project/wavelength_sweeps/sg_rr_100_025 2023-12-11 14-23-14.csv"
 transmission_data = pd.read_csv(file_path)
 transmission_data.plot()
+
 
 
