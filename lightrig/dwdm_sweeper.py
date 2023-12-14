79a8746d36acfdfc040eedc7e3b5b0684ada85eb
Tamanna Dasanjh <bp22325@bristol.ac.uk>
2023-12-14 15:06:33 +0000

    change powermeter class for Watt ouput create dwdm_sweeper

---
+++ lightrig/dwdm_sweeper.py
@@ -0,0 +1,80 @@
+"""
+Module for sweeping laser frequencies into a device and measuring
+corresponding power output.
+"""
+
+import datetime
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from laser import Laser
+from powermeter import Powermeter
+
+
+def timestamp():
+    """Time stamping for data save"""
+    return datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+
+
+class Sweeper:
+    def __init__(self, laser: Laser, powermeter: Powermeter):
+        self.laser = laser
+        self.powermeter = powermeter
+
+    def sweep(self, save_path, device_name, start=1540, end=1570, step=0.1):
+        """
+        start:          int, float
+                        Initial wavelength of sweep in nm.
+
+        end:            int, float
+                        Final wavelength of sweep in nm.
+
+        step:           int, float
+                        Increment between each wavelength in sweep in nm.
+
+        save_path:      pathlib.Path
+                        Directory to save csv data file.
+
+        device_name:    string
+                        Name of tested device for file name.
+        """
+        # Switch on laser
+        if self.laser is not None:
+            try: self.laser.switch_on()
+            except: raise Exception("No laser connection")
+
+        # Number of wavelengths in the sweep
+        sweep_size = int((end - start) / step) + 1
+
+        # Initialise arrays to hold data
+        wavelengths = np.linspace(start, end, sweep_size)
+        transmission = np.zeros(wavelengths.shape)
+
+        # Create plot
+        figure, ax = plt.subplots(figsize=(10, 8))
+        line1, = ax.plot(wavelengths, transmission)
+        plt.title("Transmission Power", fontsize=20)
+        plt.xlabel("wavelength / nm")
+        plt.ylabel("transmission / uW")
+
+        # Scan wavelengths and collect data
+        for i, wavelength in enumerate(wavelengths):
+            self.laser.set_wavelength(wavelength)
+            time.sleep(0.2) # time for laser to adjust and stabilise
+            transmission[i] = self.powermeter.measure()
+
+            # Update graph
+            if i % 10 == 0:
+                line1.set_ydata(transmission)
+                figure.canvas.draw()
+                figure.canvas.flush_events()
+
+        # Combine wavelengths and power into 2 x n array then save.
+        data = np.stack((wavelengths, transmission), axis=1)
+        file_path = save_path / f"{device_name} {timestamp()}.csv"
+        np.savetxt(file_path, data, delimiter=',')
+
+        plt.plot(wavelengths, transmission)
+        plt.show()
---
+++ lightrig/powermeter_watts.py
@@ -0,0 +1,45 @@
+import math
+import time
+
+import numpy as np
+import pyvisa
+
+
+class Powermeter:
+    """
+	Class which handles Thorlabs powermeter setup and user interfacing.
+
+	serial = None              Photodiode serial e.g. 'PM000192
+	"""
+    def __init__(self, serial="P0001012"):
+        # Defaults
+        self.model = "PM100D"
+        self.serial = serial
+        self.unit = "dBm"
+
+        rm = pyvisa.ResourceManager('@py')
+        resources = rm.list_resources()
+
+        for resource in resources:
+            if self.serial in resource:
+                self.powermeter = rm.open_resource(
+                    resource, read_termination='\n')
+                print(f"Powermeter is {resource}")
+
+    def measure(self):
+        """
+        Read power, convert Watts to dBm, return value.
+        """
+        try:
+            result_w = float(self.powermeter.query('measure:power?'))
+            return result_w
+        except:
+            return np.NaN
+
+    def set_wavelength(self, wl_nm):
+        self.powermeter.write(f"sense:correction:wav {wl_nm}")
+        time.sleep(0.01)
+
+    def get_wavelength(self):
+        r = float(self.powermeter.query('sense:correction:wav?')) / 1e-9
+        return r
