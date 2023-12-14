e9ad90362cf9a308794716580ce27a6ab3edfec9
Tamanna Dasanjh <bp22325@bristol.ac.uk>
2023-12-14 14:44:37 +0000

    new data taken on 13 Dec

--- lightrig/sweep_wavelengths.py
+++ lightrig/sweep_wavelengths.py
@@ -12,7 +12,7 @@ from sweeper import Sweeper
 
 # Wavelength sweep parameters
 
-device_name = "sg_rr_52_025"
+device_name = "jt_gclb_w_500nm"
 start_wavelength = 1520
 end_wavelength = 1580
 wavelength_step = 0.01
