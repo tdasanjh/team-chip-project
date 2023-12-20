45f60b7ef0760e334377cf426679ce7d52709bdf
Tamanna Dasanjh <tam.dasanjh@gmail.com>
2023-12-20 11:22:24 +0000

    fixing so first row of data is read

--- data_analysis/data_analysis_funcs.py
+++ data_analysis/data_analysis_funcs.py
@@ -14,7 +14,7 @@ def get_wavlen_and_pow_arrays(data_filepath):
     # to get an array of wavelengths and an array of powers from csv
     # takes in full csv filepath with wavelength in first column and power in second
     #returns a tuple of power and wavelength arrays
-    data_analyse=pd.read_csv(data_filepath)
+    data_analyse=pd.read_csv(data_filepath, header=None)
     data_analyse_array=data_analyse.to_numpy(copy=True)
 
     wavelengths=data_analyse_array[:,0]
