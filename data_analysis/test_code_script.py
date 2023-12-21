8c0c4b9f8e65acea7da239243bd31c8f7f13c8bf
Tamanna Dasanjh <tam.dasanjh@gmail.com>
2023-12-21 18:18:57 +0000

    just tweaking dist_use to see if it changes the result

--- data_analysis/test_code_script.py
+++ data_analysis/test_code_script.py
@@ -71,7 +71,8 @@ wvlen_peak_search=np.linspace(2*np.pi,4*np.pi,101)
 wvlen_step_size=2*np.pi/100
 approx_fsr=2*(np.pi/8)
 promin_use=0.4
-dist_use=(approx_fsr/2)/wvlen_step_size
+#dist_use=(approx_fsr/2)/wvlen_step_size
+dist_use=1
 power_peak_search=-np.sin(8*wvlen_peak_search)
 plt.plot(wvlen_peak_search,power_peak_search)
 plt.show()
@@ -81,6 +82,8 @@ print(peak_widths)
 peak_width_heights=peak_width_data[1]
 peak_width_lefts=peak_width_data[2]
 peak_width_rights=peak_width_data[3]
+print(peak_width_rights)
+print(peak_width_lefts)
 daf.plot_lines_FWHM(peak_width_heights,peak_width_lefts,peak_width_rights,wvlen_peak_search,power_peak_search)
 mean_FWHM,FWHM_error=daf.fsr_avg_and_error(peak_widths)
 print(mean_FWHM)
